4736641d7423ee827b72868426814be193a51672
Narayana R <rnarayana@gmail.com>
2013-05-16 10:46:28 +0530

    Initial checkin, POC

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jevents::Application.config.secret_token = '47edd81cbe2c2f15b4d39ebaf1a674ad8ca1e9eb5587134491c7f0d9ca01cf81ca667ae4de22f7a92e5ebb62b77c5ae7686e17dde6e0a4e13ae31f1abf984b25'
