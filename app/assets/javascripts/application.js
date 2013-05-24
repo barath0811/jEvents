e90a36e6d02304ec665f620ec330e6f5fe865546
Barath <barath0811@gmail.com>
2013-05-24 16:07:15 +0530

    Adding some styles for results

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,10 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+function textChanged(areas){
+	var searchKey = document.getElementById("areaSearchKey").value.toLowerCase();
+	for (var i = 0; i < areas.length; i++) {
+		var element = areas[i];
+		if (element.toLowerCase().indexOf(searchKey) !=-1)
+			document.getElementById(element).style.display="block";
+		else
+			document.getElementById(element).style.display="none";
+	}
+}
