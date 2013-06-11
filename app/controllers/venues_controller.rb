96c79ad659d0c3a74f2c89a18e36d4d49b19d7c3
Barath <barath0811@gmail.com>
2013-06-11 18:16:45 +0530

    Adding columns and search query

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -164,9 +164,9 @@ class VenuesController < ApplicationController
   end
 
   def search
-    query = SearchCriteria.new
+    
     @query = SearchCriteria.new
-    @query.eventType = nil
+    
     unless params[:eventType].nil?
       @query.eventType = params[:eventType]
     end
@@ -175,27 +175,23 @@ class VenuesController < ApplicationController
       @query.areas = params[:areas].split(',')
     end
 
-    unless params[:areas].nil?
-      query.areas = params[:areas]
-    end
-
     unless params[:budget].nil?
-      query.areas = params[:budget]
+      @query.budget = params[:budget]
     end
 
     unless params[:amenities].nil?
-      query.areas = params[:amenities]
+      @query.amenities = params[:amenities]
     end
 
     unless params[:capacities].nil?
-      query.areas = params[:capacities]
+      @query.capacity = params[:capacities]
     end
 
     unless params[:meal].nil?
-      query.areas = params[:meal]
+      @query.mealOptions = params[:meal]
     end
 
-    @venue = Venue.search(query)
+    @venue = Venue.search(@query)
 
     unless params[:po].nil?
       respond_to do |format|
