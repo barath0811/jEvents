c3ad8bca1dcd86788fd24742f870babb6d2c220c
Narayana R <rnarayana@gmail.com>
2013-06-16 02:17:09 +0530

    seating arrangements added

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -45,16 +45,4 @@ module VenuesHelper
 
 	end
 
-	def getOptions(venueType)
-		file = File.new("app/assets/xml/Properties.xml")
-		doc = REXML::Document.new file
-		options = Array.new
-
-		doc.elements.each('//'+venueType+'/option') do |ele|
-			options << ele.text
-		end
-
-		return options
-	end
-
 end
