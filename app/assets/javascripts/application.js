0ddc3b9a874674569ce3a08bd827e61fa74b6f66
Narayana R <rnarayana@gmail.com>
2013-06-20 22:03:01 +0530

    Add bootstrap normally instead of thru application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require_tree ./vendor/
 //= require rails.validations
 //= require_tree .
-*= require twitter/bootstrap/responsive
