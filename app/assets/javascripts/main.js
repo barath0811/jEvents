dab574d27f6bd03299fb9ebc9a11331dc18d6ca5
Narayana R <rnarayana@gmail.com>
2013-06-26 22:54:01 +0530

    fix header

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -35,10 +35,6 @@ $(document).on('ajax:success', function(){
 	}
 });
 
-// $(document).on('ajax:success', 'form.ajax_form', function() {
-// 	//processPendingNotifications();
-// });
-
 $(document).ajaxComplete(function(event, request){
 	//type = request.getResponseHeader("X-Message-Type");
 	showNotification(request.getResponseHeader("X-Message"));
@@ -55,19 +51,4 @@ var showNotification = function(text)
 	.slideDown(1000)
 	.delay(2000)
 	.fadeOut(1500);
-}
-
-
-var googleSigninCallback = function(authResult) {
-	if (authResult['access_token']) {
-    // Successfully authorized
-    // Hide the sign-in button now that the user is authorized, for example:
-    document.getElementById('signinButton').setAttribute('style', 'display: none');
-} else if (authResult['error']) {
-    // There was an error.
-    // Possible error codes:
-    //   "access_denied" - User denied access to your app
-    //   "immediate_failed" - Could not automatically log in the user
-    // console.log('There was an error: ' + authResult['error']);
-}
 }
