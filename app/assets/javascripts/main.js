d35582bb481b38c267fcdd4a7f0ecb11160c0748
Narayana R <rnarayana@gmail.com>
2013-06-26 18:56:19 +0530

    Align FB like

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,8 @@
 JOLLY = {}
 JOLLY.venues = {}
 
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
 $(document).ready(function(){
 	$('.selectpicker').selectpicker({
 		style: 'btn-inverse btn-large'
