6164229bd651bee0c9feb77c1fd2a5e226ab86f3
Narayana R <rnarayana@gmail.com>
2013-06-30 11:25:57 +0530

    admin ac

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 
 Role.create([{name: 'Admin'}, {name: 'Customer'}, {name: 'VenueOwner'}])
 
-User.create([{email:'rnarayana@live.com', password:'pinngs7123', password_confirmation:'pinngs7123'}])
+User.create([{email:'rnarayana@live.com', password:'pinngs7123', password_confirmation:'pinngs7123', role_ids:'1'}])
